ff42e7a350ca5a795f1b6151cee4b0cc5f566214
bobannbg <slobodan_krasavcevic@yahoo.com>
2021-01-29 08:47:34 +0100

    small fix in configure_db.bat file

--- backend/bin/configure_db.bat
+++ backend/bin/configure_db.bat
@@ -7,7 +7,7 @@ echo "Configuring dragonstacknewdb"
 dropdb -U node_user_new dragonstacknewdb
 createdb -U node_user_new dragonstacknewdb
 
-psql -U node_user dragonstacknewdb < .\\bin\\sql\\generation.sql
-psql -U node_user dragonstacknewdb < .\\bin\\sql\\dragon.sql
+psql -U node_user_new dragonstacknewdb < .\\bin\\sql\\generation.sql
+psql -U node_user_new dragonstacknewdb < .\\bin\\sql\\dragon.sql
 
 echo "dragonstackdb configured"
